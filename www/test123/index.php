821d3960fbc70dd221940728eb11c8d41350d083
Hamad Abdullha <bo3bdo@hotmail.com>
2025-06-11 01:08:21 +0300

    temp

--- www/test123/index.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE html> 
-<html><head><title>test123</title></head> 
-<body style="text-align:center;padding:50px;font-family:Arial"> 
-<h1>test123</h1> 
-<p>الموقع يعمل! الوقت: <?php echo date('Y-m-d H:i:s'); ?></p> 
-<p>زر: <a href="http://test123.test">test123.test</a></p> 
-</body></html> 
